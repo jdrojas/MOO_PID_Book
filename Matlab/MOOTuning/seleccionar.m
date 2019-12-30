adbe78c30e61833ef98f4327f55364d7a0f35a81
José David <jose.rojas_f@ucr.ac.cr>
2019-12-30 13:10:29 -0600

    Actualización de la interfaz a App Designer, trabajando en la interfaz

--- Matlab/MOOTuning/seleccionar.m
+++ Matlab/MOOTuning/seleccionar.m
@@ -2,10 +2,10 @@ function [JdiVec,JdoVec,JrVec,KpVec,TiVec,TdVec,betaVec]=seleccionar(t0,a,Ms)
 % Devuelve los vectores de b�squeda, de acuerdo con las caracter�sticas de
 % la planta
 archivo=['ParetoX_a',num2str(a),'_to',num2str(t0),'_ms',num2str(Ms),'.csv'];
-%folder=['./Ms',num2str(Ms)]; %comentar después
-%cd(folder) % comentar despues
+folder=['./Ms',num2str(Ms)]; %comentar después
+cd(folder) % comentar despues
 M=csvread(archivo,1,0);
-%cd ..% comentar despues
+cd ..% comentar despues
 JdiVec=M(:,10);
 JdoVec=M(:,11);
 JrVec=M(:,12);
