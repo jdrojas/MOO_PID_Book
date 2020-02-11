dd94203dcb7f9dff5c8f416d149191e6f4a548e0
José David <jose.rojas_f@ucr.ac.cr>
2020-02-10 23:06:01 -0600

    Se arregló el MATLAB con lo que dijo don Víctor del heater, pero aún así, la parte de la válvula está dando problemas

--- Matlab/Examples/Heater/ParamInit.m
+++ Matlab/Examples/Heater/ParamInit.m
@@ -4,7 +4,7 @@ rhoC = 800; %heating fluid density, ρ c = 800 kgm −3 ,
 D = 0.30; %tank diameter, D = 0.30 m,
 A = pi*D^2/4;%tank inside section area, A = π D 2 /4 m 2 ,
 Hc = 0.6; %heating jacket height, H c = 0.60 m,
-Ac = pi*D^2*Hc + pi*D^2/4;% jacket heat transfer area, A c = π D H c + π D 2 /4 m 2 ,
+Ac = pi*D*Hc + pi*D^2/4;% jacket heat transfer area, A c = π D H c + π D 2 /4 m 2 ,
 Cp = 4190; % tank fluid heat capacity, C p = 4190 Jkg −1◦ C −1 ,
 Cpc = 2400; %heating fluid heat capacity, C pc = 2400 Jkg −1◦ C −1 ,
 g = 9.8; %gravity constant, g = 9.8 ms −2 ,
@@ -19,7 +19,7 @@ KxT = 0.01; %temperature control valve stem constant, K x T = 0.01/%,
 Pcp = 414;% : heating fluid pump discharge pressure, P cp = 414 kpa,
 Pcr = 138;% : heating fluid system return pressure, P cr = 138 kpa;
 Rc = 5.5e7; %heating system pipe nominal flow resistance, R c = 5.5 × 10 7 kPa/(m 3 /s) 2
-RvT = 50; %temperature control valve rangeability, R vT = 50;
+RvT = 0.2579; %50; %temperature control valve rangeability, R vT = 50;
 Tsp = 38; %temperature controller set-point, T sp = 38 ◦ C
 TL = 2; %level transmitter time constant, T L = 2 s,
 TT = 15; %temperature transmitter time constant, T T = 15 s,
@@ -31,15 +31,15 @@ Vc = Hc*pi*((D+2*Wc)^2 - D^2)/4 + Wc*pi*(D + 2*Wc)^2/4; % heat jacket volume,
 % Initial conditions of the states for the operating point
 T0  = 38;
 H0 = 0.7;
-Tco0 = 108.07;
+Tca0 = 214.03;
 YL0 = 87.48;
 YT0 = 76;
 XL0 = 0.7172;
-XT0 = 0.89;
+XT0 = 0.344;
 % Inputs and disturbances for the operating point
 UL0 = 71.72;
-UT0 = 89;
+UT0 = 34.40;%89;
 Tin = 24; % fluid inlet temperature, 22 ◦ C ≤ T i ≤ 26 ◦ C, temperature T i n = 24 ◦ C,
-Tci = 900; %heating fluid inlet temperature, T ci = 320 ◦ C,
+Tci = 320; %900; %heating fluid inlet temperature, T ci = 320 ◦ C,
 Qin = 7e-4; % tank inlet fluid flow rate, 6 × 10 −4 m 3 s −1 ≤ Q i ≤ 7.5 × 10 −4 m 3 s −1 ,
             % normal tank inlet fluid flow rate Q i n = 7 × 10 −4 m 3 s −1 ,
